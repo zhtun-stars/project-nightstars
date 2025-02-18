c43d0d243a692bb95c27236ad5cb9846921a7d85
Zaw Htun <zhtun@stars.ca>
2025-02-18 12:02:11 -0700

    if applied, will be
    - [x] added proper background
    - [x] replaced with stars logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,32 @@
 <template>
   <ClientOnly>
-    <video autoplay muted loop id="myVideo">
-      <source src="@/assets/bg.mp4" type="video/mp4" />
-    </video>
+    <div class="overflow-hidden h-[100vh] max-h-[100vh]">
+      <video autoplay muted loop id="myVideo" class="w-full">
+        <source
+          src="https://stars.ca/wp-content/uploads/2023/04/NTF-silent-web-banner.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </div>
     <div
       class="h-[500px] rounded-md fixed w-[800px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 \ flex bg-[--star-color] shadow-md"
     >
       <div class="flex-1 px-11 py-14 flex h-full flex-col gap-12">
-        <p class="text-5xl text-[white] font-bold">STARS</p>
+        <p class="text-5xl text-[white] font-bold">
+          <img
+            src="@/assets/STARS-White-RGB.png"
+            alt="Stars Logo"
+            class="w-[150px]"
+          />
+        </p>
         <p class="flex-auto flex-grow-1 text-[white] text-justify px-6">
           STARS is a lifeline for patients in rural, remote, and indigenous
           communities across Western Canada.
         </p>
-        <a class="text-[white] pb-6 px-6" target="_blank" href="https://stars.ca/who-we-are/our-north-star/"
+        <a
+          class="text-[white] pb-6 px-6"
+          target="_blank"
+          href="https://stars.ca/who-we-are/our-north-star/"
           >Learn More <SquareArrowOutUpRight class="inline-block"
         /></a>
       </div>
